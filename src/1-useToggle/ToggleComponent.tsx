ff37a654f2aade0d2bb3f46d1e5e400ff6525105
Lukáš Sukeník <lukas.lukyn.sukenik@gmail.com>
2023-07-18 19:40:17 +0200

    feat: ✨ introduce types into useDebounce (#5)
    * refactor: ♻️ wrap components into fragments
    
    * feat: ✨ introduce types into useDebounce

--- src/1-useToggle/ToggleComponent.tsx
+++ src/1-useToggle/ToggleComponent.tsx
@@ -4,12 +4,12 @@ export default function ToggleComponent() {
   const [value, toggleValue] = useToggle(false);
 
   return (
-    <div>
+    <>
       <div>{value.toString()}</div>
       <button onClick={toggleValue}>Toggle</button>
       <button onClick={() => toggleValue(true)}>Make True</button>
       <button onClick={() => toggleValue(false)}>Make False</button>
-    </div>
+    </>
   );
 }
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // import ToggleComponent from './1-useToggle/ToggleComponent';
-import TimeoutComponent from './2-useTimeout/TimeoutComponent';
-// import DebounceComponent from './3-useDebounce/DebounceComponent';
+// import TimeoutComponent from './2-useTimeout/TimeoutComponent';
+import DebounceComponent from './3-useDebounce/DebounceComponent';
 // import UpdateEffectComponent from "./4-useUpdateEffect/UpdateEffectComponent"
 // import ArrayComponent from "./5-useArray/ArrayComponent"
 // import PreviousComponent from "./6-usePrevious/PreviousComponent"
@@ -31,8 +31,8 @@ import TimeoutComponent from './2-useTimeout/TimeoutComponent';
 
 function App() {
   // return <ToggleComponent />;
-  return <TimeoutComponent />;
-  // return <DebounceComponent />;
+  // return <TimeoutComponent />;
+  return <DebounceComponent />;
   // return <UpdateEffectComponent />
   // return <ArrayComponent />
   // return <PreviousComponent />
