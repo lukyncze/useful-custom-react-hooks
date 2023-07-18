e1862a70fb6e46a13ad8fdc769a8dd0d6af11616
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2023-07-18 19:38:11 +0200

    refactor: ♻️ wrap components into fragments

--- src/2-useTimeout/TimeoutComponent.tsx
+++ src/2-useTimeout/TimeoutComponent.tsx
@@ -6,12 +6,12 @@ export default function TimeoutComponent() {
   const {clear, reset} = useTimeout(() => setCount(0), 1000);
 
   return (
-    <div>
+    <>
       <div>{count}</div>
       <button onClick={() => setCount(c => c + 1)}>Increment</button>
       <button onClick={clear}>Clear Timeout</button>
       <button onClick={reset}>Reset Timeout</button>
-    </div>
+    </>
   );
 }
 
