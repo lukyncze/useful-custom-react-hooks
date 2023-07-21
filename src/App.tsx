4b9ee872af5a67b649e35fea18bcd206773ea56c
Lukáš Sukeník <lukas.lukyn.sukenik@gmail.com>
2023-07-21 16:45:06 +0200

    feat: ✨ introduce types into useArray (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 // import ToggleComponent from './1-useToggle/ToggleComponent';
 // import TimeoutComponent from './2-useTimeout/TimeoutComponent';
 // import DebounceComponent from './3-useDebounce/DebounceComponent';
-import UpdateEffectComponent from './4-useUpdateEffect/UpdateEffectComponent';
-// import ArrayComponent from "./5-useArray/ArrayComponent"
+// import UpdateEffectComponent from './4-useUpdateEffect/UpdateEffectComponent';
+import ArrayComponent from "./5-useArray/ArrayComponent"
 // import PreviousComponent from "./6-usePrevious/PreviousComponent"
 // import StateWithHistoryComponent from "./7-useStateWithHistory/StateWithHistoryComponent"
 // import StorageComponent from "./8-useStorage/StorageComponent"
@@ -33,8 +33,8 @@ function App() {
   // return <ToggleComponent />;
   // return <TimeoutComponent />;
   // return <DebounceComponent />;
-  return <UpdateEffectComponent />;
-  // return <ArrayComponent />
+  // return <UpdateEffectComponent />;
+  return <ArrayComponent />
   // return <PreviousComponent />
   // return <StateWithHistoryComponent />
   // return <StorageComponent />
