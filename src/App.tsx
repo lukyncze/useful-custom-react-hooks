5b59f027bdbeb6c7c1f71cf3d96fe1b104c39995
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2023-07-29 07:42:56 +0200

    feat: ✨ introduce types into useFetch

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@
 // import StateWithHistoryComponent from './7-useStateWithHistory/StateWithHistoryComponent';
 // import StorageComponent from "./8-useStorage/StorageComponent"
 // import AsyncComponent from "./9-useAsync/AsyncComponent"
-import FetchComponent from './10-useFetch/FetchComponent';
-// import ScriptComponent from "./11-useScript/ScriptComponent"
+// import FetchComponent from './10-useFetch/FetchComponent';
+import ScriptComponent from "./11-useScript/ScriptComponent"
 // import DeepCompareEffectComponent from "./12-useDeepCompareEffect/DeepCompareEffectComponent"
 // import EventListenerComponent from "./13-useEventListener/EventListenerComponent"
 // import OnScreenComponentComponent from "./14-useOnScreen/OnScreenComponent"
@@ -39,8 +39,8 @@ function App() {
   // return <StateWithHistoryComponent />;
   // return <StorageComponent />
   // return <AsyncComponent />
-  return <FetchComponent />;
-  // return <ScriptComponent />
+  // return <FetchComponent />;
+  return <ScriptComponent />
   // return <DeepCompareEffectComponent />
   // return <EventListenerComponent />
   // return <OnScreenComponentComponent />
