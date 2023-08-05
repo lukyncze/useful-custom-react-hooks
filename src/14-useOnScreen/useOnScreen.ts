a4fcdcff647f498118a3c5c3a2639865749beb6a
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2023-08-05 09:12:31 +0200

    feat: ✨ introduce types into useOnScreen

--- src/14-useOnScreen/useOnScreen.ts
+++ src/14-useOnScreen/useOnScreen.ts
@@ -4,7 +4,7 @@ export default function useOnScreen(ref: RefObject<HTMLHeadingElement>, rootMarg
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    if (ref.current == null) return;
+    if (ref.current === null) return;
 
     const observer = new IntersectionObserver(([entry]) => setIsVisible(entry.isIntersecting), {
       rootMargin,
@@ -13,7 +13,7 @@ export default function useOnScreen(ref: RefObject<HTMLHeadingElement>, rootMarg
     observer.observe(ref.current);
 
     return () => {
-      if (ref.current == null) return;
+      if (ref.current === null) return;
       observer.unobserve(ref.current);
     };
   }, [ref.current, rootMargin]);
